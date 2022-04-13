f469fcd0d1f7c490722ef43f5fc0c6aaba624648
arjun <arjun259194@gmail.com>
2022-04-13 22:28:47 +0530

    theme toggle,sidebar toggle✔✔✔ next adding random data with JS

---
+++ index.js
@@ -0,0 +1,22 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const themeToggle = document.querySelector('.theme-toggle');
+
+//*show sideBar
+menuBtn.addEventListener('click',()=>{
+  sideMenu.style.display = "block"
+})
+
+//*close side bar
+closeBtn.addEventListener('click',()=>{
+  sideMenu.style.display = "none"
+})
+
+//*change theme
+themeToggle.addEventListener('click',()=>{
+  document.body.classList.toggle('dark-theme-variables')
+
+  themeToggle.querySelector('span:nth-child(1)').classList.toggle('active');
+  themeToggle.querySelector('span:nth-child(2)').classList.toggle('active');
+})
